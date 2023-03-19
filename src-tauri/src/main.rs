c8f70fb761ccaa1d78bdb61c5a4d7740fd69626f
pingfury <pingfury@outlook.com>
2023-03-19 16:01:27 +0800

    fix ui

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,30 +1,28 @@
-
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
 
 #[tauri::command]
-fn rewrite(text: &str) -> String {
-    let token = match std::env::var("API2D_API_KEY"){
-        Ok(val) => format!("Bearer {}", val),
-        Err(_) => "get env err".to_string(),
-    };
-    println!("{}", token);
+fn rewrite(text: &str, apikey: &str) -> String {
+    let token = format!("Bearer {}", apikey);
+    let content = format!("请润色这段内容, 内容: {}", text);
     let body = ureq::post("https://openai.api2d.net/v1/chat/completions")
         .set("Content-Type", "application/json")
         .set("Authorization", &token)
         .send_json(ureq::json!({
             "model": "gpt-3.5-turbo",
-            "messages": [{"role": "user", "content": text}]
-        })).expect("send json err")
-        .into_string().expect("body to string err");
+            "messages": [{"role": "user", "content": content}]
+        }))
+        .expect("send json err")
+        .into_string()
+        .expect("body to string err");
     println!("{}", body);
     body
 }
 
 fn main() {
-tauri::Builder::default()
+    tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![rewrite])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
